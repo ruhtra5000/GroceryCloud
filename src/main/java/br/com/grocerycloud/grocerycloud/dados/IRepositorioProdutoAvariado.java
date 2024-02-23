1d34d7b8088db811084589612d01dc3abc91be61
Guy176 <guy.paes176@gmail.com>
2024-02-23 14:01:30 -0300

    Produto e ProdutoAvariado (Associação)
    Lógica de Produto e ProdutoAvariado inserida, com este sendo usado como associação de Produto.

--- src/main/java/br/com/grocerycloud/grocerycloud/dados/IRepositorioProdutoAvariado.java
+++ src/main/java/br/com/grocerycloud/grocerycloud/dados/IRepositorioProdutoAvariado.java
@@ -1,4 +1,5 @@
 package br.com.grocerycloud.grocerycloud.dados;
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,15 +9,18 @@ import br.com.grocerycloud.grocerycloud.negocio.entidade.ProdutoAvariado;
 
 /**
  * Interface que representa o repositório de produtos avariados e seus métodos.
+ * 
  * @author Guilherme Paes Cavalcanti
  * @category Repositório de dados
  */
 
 @Repository
-public interface IRepositorioProdutoAvariado extends JpaRepository <ProdutoAvariado, Long> {
-	
+public interface IRepositorioProdutoAvariado extends JpaRepository<ProdutoAvariado, Long> {
+
 	public ProdutoAvariado findById(long id);
-	public List<ProdutoAvariado> findAllByNome(String nome);
+
+	public List<ProdutoAvariado> findAllByNomes(String nome);
+
 	public List<ProdutoAvariado> findAllByCategoria(String categoria);
-	
+
 }
--- src/main/java/br/com/grocerycloud/grocerycloud/negocio/entidade/ProdutoAvariado.java
+++ src/main/java/br/com/grocerycloud/grocerycloud/negocio/entidade/ProdutoAvariado.java
@@ -6,29 +6,32 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-public class ProdutoAvariado extends Produto {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class ProdutoAvariado {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long idAvariado;
-    private int qtdeAvariados;
-    private Date dataAvariado;
-    
-    public ProdutoAvariado(Produto produto, int qtdeAvariados, Date dataAvariado){
-    	super(produto.getNome(), produto.getCategoria(), produto.getQtdeEstoque(),produto.getPreco(), produto.getPrecoDesconto());
-    	this.qtdeAvariados = qtdeAvariados;
-    	this.dataAvariado = dataAvariado;
-    }
-    
+	private Produto produto;
+	private int qtdeAvariados;
+	private Date dataAvariado;
+
+	public ProdutoAvariado(Produto produto, int qtdeAvariados, Date dataAvariado) {
+		this.produto = produto;
+		this.qtdeAvariados = qtdeAvariados;
+		this.dataAvariado = dataAvariado;
+	}
+
 	public long getIdAvariado() {
 		return idAvariado;
 	}
+
 	public void setIdAvariado(long idAvariado) {
 		this.idAvariado = idAvariado;
 	}
-	
+
 	public int getQtdeAvariados() {
 		return qtdeAvariados;
 	}
+
 	public void setQtdeAvariados(int qtdeAvariados) {
 		this.qtdeAvariados = qtdeAvariados;
 	}
@@ -36,10 +39,16 @@ public class ProdutoAvariado extends Produto {
 	public Date getDataAvariado() {
 		return dataAvariado;
 	}
+
 	public void setDataAvariado(Date dataAvariado) {
 		this.dataAvariado = dataAvariado;
 	}
-    
-    
-	
+
+	public Produto getProduto() {
+		return this.produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
 }
